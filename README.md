098762bedd7f069dc9305c0025d8fc84fa3fa1db
yuiiio <atbjyk@protonmail.com>
2023-08-16 09:53:00 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# rp2040-rs
+
+```
+rustup target add thumbv6m-none-eabi
+cargo build
+cargo run --release
+```
+
+# LICENSE
+GPL-3.0
---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+# from https://github.com/hikalium/keyball-rs/blob/main/deploy.sh
+# thankyou @hikalium !
+ELF_PATH="$1"
+if [ -z "${SSH_OPENOCD}" ]
+then
+	elf2uf2-rs -d "${ELF_PATH}"
+else
+	# `SSH_OPENOCD=pi4 cargo run --release` to deploy the binary via openocd on a host ${SSH_OPENOCD}
+	scp ${ELF_PATH} ${SSH_OPENOCD}:~/target.elf && \
+		ssh ${SSH_OPENOCD} -- openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c '"targets rp2040.core0; program target.elf verify reset exit"'
+fi
