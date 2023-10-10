0f83f576b9dba9451bd97b25e86f69209ecedc7e
yuiiio <atbjyk@protonmail.com>
2023-10-10 19:14:09 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ then
 	elf2uf2-rs -d "${ELF_PATH}"
 else
 	# `SSH_OPENOCD=pi4 cargo run --release` to deploy the binary via openocd on a host ${SSH_OPENOCD}
-	scp ${ELF_PATH} ${SSH_OPENOCD}:~/target.elf && \
-		ssh ${SSH_OPENOCD} -- openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c '"targets rp2040.core0; program target.elf verify reset exit"'
+	scp -O ${ELF_PATH} ${SSH_OPENOCD}:~/target.elf && \
+		ssh ${SSH_OPENOCD} -- sudo openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c '"targets rp2040.core0; program target.elf verify reset exit"'
 fi
